0987b440966f8666016858e34b6f65d4f2a9ca89
Luiz Felipe <mail.lfellipe@gmail.com>
2024-09-14 10:56:45 -0300

    commit 14/09

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-});
+    const nomeUsuario = document.getElementById('nomeUsuario')
+    const indexProximo = document.getElementById('indexProximo')
 
- 
+    indexProximo.addEventListener('click', function() {
+        const nomeUsuario = nomeUsuario.value
+    })
+
+
+    
+});
